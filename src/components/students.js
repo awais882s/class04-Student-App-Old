34d0d151e7be57e728df3c3d5a1ebb684f209f63
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-22 23:00:41 +0500

    Update students.js

--- src/components/students.js
+++ src/components/students.js
@@ -1,171 +1,176 @@
 import { useState } from "react";
 import { data } from "./data";
-import StudentList from './studentsList';
+import StudentList from "./studentsList";
 
 function Students() {
-    const [students, setStudents] = useState(data)
-    const [errorMessage, setMessage] = useState("");
-    const [name, setName] = useState("")
-    const [Batch, setBatch] = useState("")
-    const [roll, setRoll] = useState("")
-    const [stuClass, setStuClass] = useState("")
-    const [flag, setFlag] = useState(false);
-    const [updatedIndex, setUpdatedIndex] = useState(0)
+  const [students, setStudents] = useState(data);
+  const [errorMessage, setMessage] = useState("");
+  const [name, setName] = useState("");
+  const [Batch, setBatch] = useState("");
+  const [roll, setRoll] = useState("");
+  const [stuClass, setStuClass] = useState("");
+  const [flag, setFlag] = useState(false);
+  const [updatedIndex, setUpdatedIndex] = useState(0);
 
-    // const inputHandlerName = (e)=>{
-    //     console.log("e", e.targe.name);
-    //     console.log("E ", e.target.value)
-    // }\
+  // const inputHandlerName = (e)=>{
+  //     console.log("e", e.targe.name);
+  //     console.log("E ", e.target.value)
+  // }\
 
+  // const deleteHandler = (stuname) => {
+  //     console.log('name', stuname);
 
-    // const deleteHandler = (stuname) => {
-    //     console.log('name', stuname);
+  //    let newStudents =  students.filter((student)=>{
+  //      if (student.name !== stuname ) {
+  //          return student;
+  //      }
+  //    });
 
-    //    let newStudents =  students.filter((student)=>{
-    //      if (student.name !== stuname ) {
-    //          return student;
-    //      }
-    //    });
+  //    setStudents([...newStudents]);
 
-    //    setStudents([...newStudents]);
+  //    console.log("newStudents", newStudents);
+  // }
 
-    //    console.log("newStudents", newStudents);
-    // } 
+  const deleteHandler = (index) => {
+    // console.log('name', stuname);
 
+    let newStudents = students.filter((student, i) => {
+      if (i !== index) {
+        return student;
+      }
+    });
 
-    const deleteHandler = (index) => {
-        // console.log('name', stuname);
+    setStudents([...newStudents]);
 
-        let newStudents = students.filter((student, i) => {
-            if (i !== index) {
-                return student;
-            }
-        });
+    console.log("newStudents", newStudents);
+  };
 
-        setStudents([...newStudents]);
+  const updateHandler = (student, index) => {
+    console.log("need to update stu ", student);
+    setUpdatedIndex(index);
+    setName(student.name);
+    setStuClass(student.class);
+    setRoll(student.roll);
+    setBatch(student.batch);
+    setFlag(true);
+  };
 
-        console.log("newStudents", newStudents);
+  const ctaHandler = () => {
+    setMessage("");
+
+    if (name != "" && Batch != "" && roll != "" && stuClass != "") {
+      let student = {
+        name,
+        batch: Batch,
+        roll,
+        class: stuClass,
+      };
+      console.log("student", student);
+
+      setStudents([...students, student]);
+      setName("");
+      setStuClass("");
+      setRoll("");
+      setBatch("");
+    } else {
+      setMessage(" Found few of params empty! Params can't be empty.");
     }
 
-    const updateHandler = (student, index) => {
+    // new data add in student list
+  };
 
-        console.log("need to update stu ", student);
-        setUpdatedIndex(index);
-        setName(student.name);
-        setStuClass(student.class);
-        setRoll(student.roll);
-        setBatch(student.batch);
-        setFlag(true);
+  const ctaUpdateHandler = () => {
+    setMessage("");
 
-    }
+    if (name != "" && Batch != "" && roll != "" && stuClass != "") {
+      let student = {
+        name,
+        batch: Batch,
+        roll,
+        class: stuClass,
+      };
+      console.log("student", student);
 
-    const ctaHandler = () => {
-        setMessage("")
-
-        if (name != "" && Batch != "" && roll != "" && stuClass != "") {
-            let student = {
-                name,
-                batch: Batch,
-                roll,
-                class: stuClass
-            }
-            console.log("student", student);
-
-
-
-            setStudents([...students, student]);
-            setName("");
-            setStuClass("");
-            setRoll("");
-            setBatch("");
-
+      let updateStudents = students.map((stu, index) => {
+        if (updatedIndex === index) {
+          return student;
+        } else {
+          return stu;
         }
-        else {
-            setMessage(" Found few of params empty! Params can't be empty.")
-        }
+      });
 
-
-        // new data add in student list
+      setStudents([...updateStudents]);
+      setName("");
+      setStuClass("");
+      setRoll("");
+      setBatch("");
+      setFlag(false);
+    } else {
+      setMessage(" Found few of params empty! Params can't be empty.");
     }
+  };
 
-
-    const ctaUpdateHandler = () => {
-        setMessage("")
-
-        if (name != "" && Batch != "" && roll != "" && stuClass != "") {
-            let student = {
-                name,
-                batch: Batch,
-                roll,
-                class: stuClass
-            }
-            console.log("student", student);
-
-            let updateStudents = students.map((stu,index) => {
-                if (updatedIndex === index) {
-                    return student
-                }
-            else {
-            return stu;
-        }
-    })
-
-    setStudents([...updateStudents]);
-    setName("");
-    setStuClass("");
-    setRoll("");
-    setBatch("");
-    setFlag(false);
-
-}
-    else {
-    setMessage(" Found few of params empty! Params can't be empty.")
-}
-    }
-
-
-
-
-return (
+  return (
     <div>
-        <h1>New Student Add</h1>
-        <input type="text" value={name} name="name" placeholder="name" onChange={(e) => setName(e.target.value)} />
-        <input type="text" value={Batch} name="s" placeholder="Batch" onChange={(e) => setBatch(e.target.value)} />
-        <input type="text" value={roll} placeholder="Roll No." onChange={(e) => setRoll(e.target.value)} />
-        <input type="text" value={stuClass} placeholder="Class" onChange={(e) => setStuClass(e.target.value)} />
+      <h1>New Student Add</h1>
+      <input
+        type="text"
+        value={name}
+        name="name"
+        placeholder="name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <input
+        type="text"
+        value={Batch}
+        name="s"
+        placeholder="Batch"
+        onChange={(e) => setBatch(e.target.value)}
+      />
+      <input
+        type="text"
+        value={roll}
+        placeholder="Roll No."
+        onChange={(e) => setRoll(e.target.value)}
+      />
+      <input
+        type="text"
+        value={stuClass}
+        placeholder="Class"
+        onChange={(e) => setStuClass(e.target.value)}
+      />
 
-        {flag ?
-            <button onClick={ctaUpdateHandler}>update</button>
-            :
-            <button onClick={ctaHandler}>submit</button>
-        }
+      {flag ? (
+        <button onClick={ctaUpdateHandler}>update</button>
+      ) : (
+        <button onClick={ctaHandler}>submit</button>
+      )}
 
-        <p style={{ backgroundColor: "red", color: 'white' }}>
-            {
-                errorMessage
-            }
-        </p>
-        <hr />
-        <h1>List of students</h1>
-        <table>
-            <tr>
-                <th>No.</th>
-                <th>Name</th>
-                <th>Batch</th>
-                <th>Roll No</th>
-                <th>class</th>
-            </tr>
+      <p style={{ backgroundColor: "red", color: "white" }}>{errorMessage}</p>
+      <hr />
+      <h1>List of students</h1>
+      <table>
+        <tr>
+          <th>No.</th>
+          <th>Name</th>
+          <th>Batch</th>
+          <th>Roll No</th>
+          <th>class</th>
+        </tr>
 
-            {
-                students.map((item, index) => {
-                    return <StudentList index={index} student={item} deleteHandler={deleteHandler} updateHandler={updateHandler} />
-                })
-            }
-
-
-        </table>
+        {students.map((item, index) => {
+          return (
+            <StudentList
+              index={index}
+              student={item}
+              deleteHandler={deleteHandler}
+              updateHandler={updateHandler}
+            />
+          );
+        })}
+      </table>
     </div>
-)
+  );
 }
 
 export default Students;
