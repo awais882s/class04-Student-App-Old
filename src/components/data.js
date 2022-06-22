4bcc251f305c381c66370d500cdb8e90ab86b753
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-06-22 22:59:36 +0500

    update names

--- src/components/data.js
+++ src/components/data.js
@@ -1,48 +1,48 @@
 export const data = [
-    {
-     id:121,
-      name: "Awais",
-      batch: "batch 3",
-      roll: "32",
-      class: "web and mobile"
-    },
-    {
-        id:233212142,
-      name: "adnan",
-      batch: "batch 3",
-      roll: "32",
-      class: "web and mobile"
-    },
+  {
+    id: 121,
+    name: "Awais",
+    batch: "batch 3",
+    roll: "32",
+    class: "web and mobile"
+  },
+  {
+    id: 233212142,
+    name: "Bilal",
+    batch: "batch 3",
+    roll: "32",
+    class: "web and mobile"
+  },
 
 
-    {
-        id:23132422,
-      name: "asif",
-      batch: "batch 3",
-      roll: "23",
-      class: "web and mobile"
-    },
-    {
-        id:23323242,
-      name: "manan",
-      batch: "batch 3",
-      roll: "2323",
-      class: "web and mobile"
-    },
-    {
-        id:2313242,
-      name: "asif",
-      batch: "batch 3",
-      roll: "23",
-      class: "web and mobile"
-    },
-    {
-        id:23323242,
-      name: "manan",
-      batch: "batch 3",
-      roll: "2323",
-      class: "web and mobile"
-    }
+  {
+    id: 23132422,
+    name: "Razzaq",
+    batch: "batch 3",
+    roll: "23",
+    class: "web and mobile"
+  },
+  {
+    id: 23323242,
+    name: "Waqar",
+    batch: "batch 3",
+    roll: "2323",
+    class: "web and mobile"
+  },
+  {
+    id: 2313242,
+    name: "Hamza",
+    batch: "batch 3",
+    roll: "23",
+    class: "web and mobile"
+  },
+  {
+    id: 23323242,
+    name: "Asad",
+    batch: "batch 3",
+    roll: "2323",
+    class: "web and mobile"
+  }
 
 
-  ]
+]
